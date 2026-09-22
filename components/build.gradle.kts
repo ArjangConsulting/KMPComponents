8e09ea18f93c53098f100daf7e1a8835e4a1eaa3
maniramezan <mani.ramezan@gmail.com>
2026-09-22 01:21:46 -0400

    fix(build): bump compileSdk to 37 for Compose 1.12.0 compatibility

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
 
     android {
         namespace = "io.github.maniramezan.kmpcomponents"
-        compileSdk = 36
+        compileSdk = 37
         minSdk = 26
         compilerOptions.jvmTarget = JvmTarget.JVM_17
         withHostTest {}
